fdfa1db303f65a096c78ff92b0be3f53115cb1bc
Mchetty <Mchetty@BUNGARBY.uni.federation.edu.au>
2018-02-20 15:50:15 +1100

    new changes

---
+++ SyncTestProject/src/syncpackage/syncdemo.java
@@ -0,0 +1,17 @@
+package syncpackage;
+
+public class syncdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		System.out.println("This is another line within the same program");
+		System.out.print("This is another way to print out a line on the console");
+		System.out.print("But Notice how the two lines join one another.");
+		System.out.print("The \n can be used to add in a line break");
+		System.out.println("println puts a line break at the end of the line.");
+		System.out.print("print does not.");
+
+	}
+
+}
